c9cfbb3afad600c2708a93f0f8ee8b799aa18b9b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-04-20 19:10:45 +0200

    extracted device file

---
+++ firmware/src/dev.h
@@ -0,0 +1,6 @@
+#ifndef DEV_H
+#define DEV_H
+
+void dev_init();
+
+#endif  // DEV_H
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,8 +1,6 @@
 #include <naos.h>
-#include <driver/gpio.h>
-#include <driver/spi_master.h>
-#include <driver/i2c.h>
 
+#include "dev.h"
 #include "sig.h"
 #include "pwr.h"
 #include "btn.h"
@@ -12,38 +10,6 @@
 #include "dat.h"
 #include "rec.h"
 #include "scr.h"
-
-static void dev_init() {
-  // install interrupt service
-  ESP_ERROR_CHECK(gpio_install_isr_service(0));
-
-  // initialize bus
-  spi_bus_config_t spi = {
-      .mosi_io_num = 23,
-      .miso_io_num = 19,
-      .sclk_io_num = 18,
-      .quadhd_io_num = -1,
-      .quadwp_io_num = -1,
-      .max_transfer_sz = 8192,
-      .flags = SPICOMMON_BUSFLAG_MASTER,
-      .intr_flags = 0,
-  };
-  ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &spi, SPI_DMA_CH_AUTO));
-
-  // install driver
-  ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
-
-  // configure I2C driver
-  i2c_config_t i2c = {
-      .mode = I2C_MODE_MASTER,
-      .sda_io_num = 21,
-      .scl_io_num = 22,
-      .sda_pullup_en = true,
-      .scl_pullup_en = true,
-  };
-  i2c.master.clk_speed = 100 * 1000;
-  ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &i2c));
-}
 
 static void setup() {
   // initialize
