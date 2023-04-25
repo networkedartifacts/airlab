e65c51adf6b512fc5b50706181e1e3115f4b0744
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-04-25 19:26:13 +0200

    show current mark

--- firmware/src/scr.c
+++ firmware/src/scr.c
@@ -502,6 +502,7 @@ static void* scr_view() {
 
     // update bar
     bar.time = scr_fmt("%02d:%02d", hour, minute);
+    bar.mark = current.mark > 0 ? scr_fmt("(M%d)", current.mark) : "";
     if (mode == 0) {
       bar.value = scr_fmt("%.0f ppm CO2", current.co2);
     } else if (mode == 1) {
