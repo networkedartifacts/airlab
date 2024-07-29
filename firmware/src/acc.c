302b67267e4416796bfe4322d9872c2df312bd57
Joël Gähwiler <joel.gaehwiler@gmail.com>
2024-07-29 20:36:14 +0200

    firmware: finished accelerometer

--- firmware/src/acc.c
+++ firmware/src/acc.c
@@ -2,6 +2,8 @@
 #include <naos/sys.h>
 #include <driver/i2c.h>
 
+#include "acc.h"
+
 #define ACC_ADDR 0x18
 #define ACC_DEBUG false
 
@@ -17,21 +19,13 @@ static void acc_read(uint8_t reg, uint8_t *buf, size_t len) {
 }
 
 static void acc_check() {
-  // read data
-  uint8_t data[6];
-  acc_read(0x04, data, 6);
-  if (ACC_DEBUG) {
-    int16_t x = (data[1] << 8) | data[0];
-    int16_t y = (data[3] << 8) | data[2];
-    int16_t z = (data[5] << 8) | data[4];
-    naos_log("acc: x=%d y=%d z=%d", x, y, z);
-  }
-
   // read orientation
   uint8_t orientation;
   acc_read(0x28, &orientation, 1);
   if (ACC_DEBUG) {
-    naos_log("acc: orientation=%02x", orientation);
+    acc_face_t face = orientation & 0b1;
+    acc_mode_t orient = (orientation >> 1) & 0b11;
+    naos_log("acc: face=%d mode=%d", face, orient);
   }
 }
 
@@ -40,14 +34,12 @@ void acc_init() {
   acc_write(0x15, 0b10000000);
   naos_delay(20);
 
+  // enable orientation detection
+  acc_write(0x30, 1);
+  acc_write(0x29, 0b11000000);
+
   // activate device
   acc_write(0x15, 0b00000001);
-
-  // set debounce counter
-  acc_write(0x30, 1);  // TODO: Properly calculate.
-
-  // enable orientation detection
-  acc_write(0x29, 0b11000000);
 
   // run check
   naos_repeat("acc", 100, acc_check);
--- firmware/src/acc.h
+++ firmware/src/acc.h
@@ -1,6 +1,18 @@
 #ifndef ACC_H
 #define ACC_H
 
+typedef enum {
+  ACC_FRONT = 0b0,
+  ACC_BACK = 0b1,
+} acc_face_t;
+
+typedef enum {
+  ACC_PORTRAIT_UP = 0b00,
+  ACC_PORTRAIT_DOWN = 0b01,
+  ACC_LANDSCAPE_RIGHT = 0b10,
+  ACC_LANDSCAPE_LEFT = 0b11,
+} acc_mode_t;
+
 void acc_init();
 
 #endif  // ACC_H
