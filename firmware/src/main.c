691673618b802ba7a21a8cef2ac2ce9cddd6e697
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-03-28 19:40:13 +0100

    firmware: expose battery and storage levels

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,6 +3,7 @@
 #include <naos/sys.h>
 
 #include <al/core.h>
+#include <al/power.h>
 
 #include "dev.h"
 #include "sig.h"
@@ -12,6 +13,16 @@
 #include "rec.h"
 #include "com.h"
 #include "scr.h"
+
+static float battery() {
+  // return battery level
+  return al_power_get().battery;
+}
+
+static void storage() {
+  // update storage metric
+  naos_set_d("storage", dat_info().usage);
+}
 
 static void setup() {
   // init core
@@ -32,19 +43,25 @@ static void setup() {
   rec_init();
   com_init();
 
-  // check storage
-  dat_info_t info = dat_info();
-  naos_log("main: space total=%lu free=%lu usage=%.1f%%", info.total, info.free, info.usage * 100.f);
+  // update storage
+  naos_repeat("storage", 1000, storage);
 
   // run screen
   scr_run();
 }
+
+static naos_param_t params[] = {
+    {.name = "storage", .type = NAOS_DOUBLE},
+};
 
 static naos_config_t config = {
     .device_type = "airlab",
     .device_version = DEV_VERSION,
     .setup_callback = setup,
     .online_callback = com_online,
+    .battery_callback = battery,
+    .parameters = params,
+    .num_parameters = sizeof(params) / sizeof(naos_param_t),
 };
 
 void app_main() {
