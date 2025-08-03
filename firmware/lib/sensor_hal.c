2df13135eaeab0614edd4cf6afc71264db9d3f6b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-08-03 21:47:16 +0200

    firmware: use fixed SCD temp. offset

--- firmware/lib/sensor_hal.c
+++ firmware/lib/sensor_hal.c
@@ -137,12 +137,8 @@ al_sensor_hal_err_t al_sensor_hal_config(al_sensor_hal_mode_t mode, int interval
   AL_CHECK(al_sensor_hal_transfer(AL_SENSOR_HAL_SCD41, 0x3f86, 0, 0, true));
   al_sensor_hal_ops.delay(500);
 
-  // apply SCD temperature offsets
-  if (mode == AL_SENSOR_HAL_LOW_POWER || mode == AL_SENSOR_HAL_MANUAL) {
-    al_sensor_hal_bw[0] = (uint16_t)(1.5 * (65535.f / 175.f));
-  } else {
-    al_sensor_hal_bw[0] = (uint16_t)(3 * (65535.f / 175.f));
-  }
+  // apply custom SCD temperature offset
+  al_sensor_hal_bw[0] = (uint16_t)(3 * (65535.f / 175.f));
   AL_CHECK(al_sensor_hal_transfer(AL_SENSOR_HAL_SCD41, 0x241d, 1, 0, false));
 
   // apply SCD sensor mode
