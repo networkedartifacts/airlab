7dec17fd4aabf4b411dcf8da688d0c160efd7a59
Joël Gähwiler <joel.gaehwiler@gmail.com>
2026-02-22 22:52:36 +0100

    plugins: make screen example configurable

--- plugins/screen/main.c
+++ plugins/screen/main.c
@@ -1,23 +1,37 @@
 #include "../al.h"
 
 int main() {
-  // collect info
+  // get config
+  char primary[32] = {0};
+  al_config_get_s("primary", primary, sizeof(primary));
+
+  // primary value
+  char pri_str[32];
+  if (strcmp(primary, "co2") == 0) {
+    float co2 = al_info(AL_INFO_SENSOR_CO2);
+    snprintf(pri_str, sizeof(pri_str), "%.0f ppm", co2);
+  } else if (strcmp(primary, "voc") == 0) {
+    float voc = al_info(AL_INFO_SENSOR_VOC);
+    snprintf(pri_str, sizeof(pri_str), "%.0f VOC", voc);
+  } else if (strcmp(primary, "nox") == 0) {
+    float nox = al_info(AL_INFO_SENSOR_NOX);
+    snprintf(pri_str, sizeof(pri_str), "%.0f NOx", nox);
+  } else {
+    snprintf(pri_str, sizeof(pri_str), "No primary");
+  }
+
+  // secondary values
   float tmp = al_info(AL_INFO_SENSOR_TEMPERATURE);
   float hum = al_info(AL_INFO_SENSOR_HUMIDITY);
-  float co2 = al_info(AL_INFO_SENSOR_CO2);
-
-  // format info
   char tmp_str[32];
   char hum_str[32];
-  char co2_str[32];
   snprintf(tmp_str, sizeof(tmp_str), "%.1f °C", tmp);
   snprintf(hum_str, sizeof(hum_str), "%.1f %%", hum);
-  snprintf(co2_str, sizeof(co2_str), "%.0f ppm", co2);
 
   // write text
   al_write(20, 20, 0, 16, 1, tmp_str, 0);
   al_write(AL_W - 20, 20, 0, 16, 1, hum_str, AL_WRITE_ALIGN_RIGHT);
-  al_write(AL_W / 2, AL_H / 2 - 12, 0, 24, 1, co2_str, AL_WRITE_ALIGN_CENTER);
+  al_write(AL_W / 2, AL_H / 2 - 12, 0, 24, 1, pri_str, AL_WRITE_ALIGN_CENTER);
 
   return 0;
 }
